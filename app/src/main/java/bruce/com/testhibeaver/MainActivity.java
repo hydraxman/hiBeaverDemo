152891e2deaf623a128803b6a9181109319688db
bsp <bsp0911932@163.com>
2017-02-25 16:46:27 +0800

    init

---
+++ app/src/main/java/bruce/com/testhibeaver/MainActivity.java
@@ -0,0 +1,15 @@
+package bruce.com.testhibeaver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("hi","hi");
+    }
+}
